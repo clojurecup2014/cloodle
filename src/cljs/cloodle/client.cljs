78fa60b685133796c7c38e570d7246b0814688a2
Janne Lemmetti <jlemmetti@gmail.com>
2014-09-27 12:41:12 +0300

    Working towards the Cloodle input form

--- src/cljs/cloodle/client.cljs
+++ src/cljs/cloodle/client.cljs
@@ -1,14 +1,54 @@
 (ns cloodle.client
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [om-tools.core :refer-macros [defcomponent]]))
 
 
-(defn widget [data owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/h1 nil (:text data)))))
+(def form-state
+  (atom
+   {
 
-(om/root widget {:text "Hello world!"}
-  {:target (. js/document (getElementById "my-app"))})
+    :name "Test Cloodle Name"
+    :description "Test Description"
+    :options []
+    :cloodle-code ""
+
+    }))
+
+(defcomponent cloodle-form [form-data owner]
+  (render [this]
+
+          (dom/div #js {:className "form-group"}
+                   (dom/form #js {:className "form-horizontal" :role "form"}
+
+                             ;; TODO: Label / Input pairs as components?
+                             ;; NAME
+                             (dom/div #js {:className "form-group"}
+
+                                      ;; TODO: Label / Input pairs as components?
+                                      (dom/label #js {:for "name"
+                                                      :className "col-sm-2 control-label"}
+                                                 "Event name")
+                                      (dom/div #js {:className "col-sm-6"}
+                                               (dom/input #js {:id "name"
+                                                               :className "form-control"
+                                                               :type "text"
+                                                               :value (:name form-data)})))
+
+                             ;; DESCRIPTION
+                             (dom/div #js {:className "form-group"}
+                                      (dom/label #js {:for "description"
+                                                      :className "col-sm-2 control-label"}
+                                                 "Event description")
+                                      (dom/div #js {:className "col-sm-10"}
+                                               (dom/input #js {:id "description"
+                                                               :className "form-control"
+                                                               :type "text"
+                                                               :value (:description form-data)})))
+                             )
+          )))
+
+(om/root cloodle-form form-state
+         {:target (. js/document (getElementById "my-app"))})
+
 
