618a5102acf4ee2719a48f72c898c81ce469e1ff
jarkkor <jarkko.rantamaki@iki.fi>
2014-09-27 09:12:21 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject cloodle "0.1.0-SNAPSHOT"
+  :description "Awesome cloodle application by Pitäjänmäki hackers!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [ring "1.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main cloodle.server
+  :ring {:handler cloodle.server/app})
+
