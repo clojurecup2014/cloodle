19e8212df24174d11ad836ff1906bec72bc850f5
Janne Lemmetti <jlemmetti@gmail.com>
2014-09-28 11:25:15 +0300

    User is shown the cloodle code after saving an event

--- src/cljs/cloodle/client.cljs
+++ src/cljs/cloodle/client.cljs
@@ -14,10 +14,9 @@
 
 (enable-console-print!)
 
-(def form-state
+(def app-state
   (atom
    {
-
     :name "Movie Night"
     :description "Let's drink beer and watch an Arnie movie"
     :options [
@@ -25,9 +24,25 @@
               {:name "Commando"}
               {:name "Conan The Barbarian"}
               {:name "Junior"}]
-    :cloodle-code ""}))
+    :cloodle-code ""
+    :saved false}))
+
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
 
 
+(defcomponent title-and-description [form-data owner]
+
+  (render-state [this {:keys [visible]}]
+
+                (if visible
+                  (dom/div nil
+
+
+                           (dom/h1 nil (:name form-data))
+                           (dom/h2 nil (:description form-data))))))
 
 (defcomponent option-container [option-data owner]
   (render-state [this {:keys [delete-chan]}]
@@ -58,6 +73,34 @@
 (defn handle-new-option-change [e owner {:keys [new-option-text]}]
   (om/set-state! owner :new-option-text (.. e -target -value)))
 
+
+;; CLOODLE CODE COMPONENT
+(defcomponent cloodle-code [code owner]
+
+
+  (render-state [this state]
+          (print (:saved state))
+          (if (:saved state)
+            (dom/div #js {:className "center-block bg-info cloodle-code-box"}
+                     (dom/h1 nil
+
+                             (dom/span nil "This event's ")
+                             (dom/span #js {:className "cloodle-name"} "Cloodle")
+                             (dom/span nil " code is"))
+
+                     (dom/div #js {:className "alert alert-success center-block" :role "alert"}
+
+                              (dom/h1 #js {:className "center-block"} code))
+
+                     (dom/h1 nil "Share it with your friends and have them vote on the options!")))))
+
+
+
+
+
+
+
+
 (defcomponent new-option-component [form-data owner]
 
   (render-state [this state]
@@ -86,6 +129,7 @@
   (select-keys state (disj (set (keys state)) :cloodle-code)))
 
 
+;; MAIN FORM COMPONENT
 (defcomponent cloodle-form [form-data owner]
 
   (init-state [_]
@@ -98,36 +142,50 @@
               (let [delete (om/get-state owner :delete-option-chan)
                     save (om/get-state owner :save-event-chan)]
 
+
+                ;; DELETE OPTION
                 (go (loop []
                       (let [option (<! delete)]
+
 
                         (om/transact! form-data :options
                                       (fn [xs] (vec (remove #(= option %) xs))))
 
                         (recur))))
 
+                ;; SAVE EVENT
                 (go (loop []
                       (let [save-event (<! save)
                             payload (state->save-payload save-event)]
 
-                        (print "Got save event: " payload)
+
 
                         (go
-                         (let [response (<! (http/post "api/event" {:json-params payload}))]
-                           (print (:status response))
-                           (print (:body response))
+                         (let [response (<! (http/post "api/event" {:json-params payload}))
+                               status (:status response)]
+
+                            (if-let [cloodle-code (if (= status 200) (:body response))]
+
+                              (do
+                                (om/transact! form-data :cloodle-code (fn [_] cloodle-code))
+                                (om/transact! form-data :saved (fn [_] true)))
+
+
+                              ;; TODO: Display errors? Or rather prevent sending this stuff via UI
+                              (print "Nope, failers"))
+
+
+
 
                            ))
 
-                        (recur))))
-
-
-                ))
+                        (recur))))))
 
   (render-state [this state]
 
           (dom/div #js {:className "form-group"}
-                   (dom/form #js {:className "form-horizontal" :role "form"}
+                   (dom/form #js {:className "form-horizontal" :role "form"
+                                  :style (display (not (:saved state)))}
 
                              ;; TODO: Label / Input pairs as components?
                              ;; NAME
@@ -156,7 +214,9 @@
                                                                :value (:description form-data)})))
 
 
+                             ;; EXISTING OPTIONS
                              (dom/div #js {:className "form-group"}
+
                                       (dom/label #js {:className "col-sm-2 control-label"}
                                                  "Options")
 
@@ -165,24 +225,45 @@
                                                              {:init-state {:delete-chan (:delete-option-chan state)}})))
 
 
-                             (om/build new-option-component
-                                       form-data
-                                       {:init-state state})
+                             ;; ADD NEW OPTION
+                               (om/build new-option-component
+                                         form-data
+                                         {:state state})
 
 
+                             ;; CREATE BUTTON
                              (dom/div #js {:className "form-group"}
 
                                       (dom/div #js {:className "col-sm-2"} nil)
                                       (dom/div #js {:className "col-sm-6"}
                                                (dom/button #js {:type "button"
                                                                 :className "btn btn-success"
-                                                                :onClick (fn [e] (put! (:save-event-chan state) @form-data)) ;; (do (put! delete-chan @option-data) false)
+                                                                :onClick (fn [e] (put! (:save-event-chan state) @form-data))
                                                                 }
                                                            (dom/span #js {:className "glyphicon glyphicon-ok"})
-                                                           (dom/span nil " Create!"))))))))
+                                                           (dom/span nil " Create!")))))
+
+                   (om/build cloodle-code (:cloodle-code form-data) {:state {:saved (:saved state)}}))))
 
 
-(om/root cloodle-form form-state
+
+(defcomponent main-page [app-state owner]
+
+  (render [this]
+    (dom/div nil
+      (om/build title-and-description
+                (select-keys app-state [:name :description])
+                {:state {:visible (:saved app-state)}})
+
+      (om/build cloodle-form
+                app-state
+                {:state {:saved (:saved app-state)}})))
+
+
+
+  )
+
+(om/root main-page app-state
          {:target (. js/document (getElementById "my-app"))})
 
 
